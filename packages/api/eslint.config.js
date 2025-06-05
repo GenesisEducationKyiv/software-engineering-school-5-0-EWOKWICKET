e32e59627b8705e5e701001e91e64f1fb6ab541b
Ivan Hushchin <oopsgu2006@gmail.com>
2025-06-05 23:36:16 +0300

    minor eslint fix

--- packages/api/eslint.config.js
+++ packages/api/eslint.config.js
@@ -2,7 +2,7 @@ import * as tseslint from 'typescript-eslint';
 
 export default [
   {
-    ignores: ['test/**'],
+    ignores: ['test/**', 'src/**/*.spec.ts'],
   },
   {
     languageOptions: {
@@ -24,7 +24,7 @@ export default [
     },
     rules: {
       '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
 ];
