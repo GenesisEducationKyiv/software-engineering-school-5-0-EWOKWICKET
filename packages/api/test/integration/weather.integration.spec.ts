2072d2a0d13aa6beb80cfa49d40751ef12b43ea4
Ivan Hushchin <oopsgu2006@gmail.com>
2025-07-02 18:00:13 +0300

    minor weather test fix

--- packages/api/test/integration/weather.integration.spec.ts
+++ packages/api/test/integration/weather.integration.spec.ts
@@ -1,41 +1,20 @@
-import { HttpService } from '@nestjs/axios';
 import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
+import { CityNotFoundException } from 'src/common/errors/city-not-found.error';
 import { ExternalApiException } from 'src/common/errors/external-api.error';
 import { appTestConfig, databaseTestConfig } from 'src/config/test.config';
 import { CurrentWeatherResponseDto } from 'src/weather/dtos/current-weather-response.dto';
 import { OpenWeatherWeatherProvider } from 'src/weather/providers/openweather.provider';
 import { WeatherApiWeatherProvider } from 'src/weather/providers/weatherapi.provider';
 import { WeatherTestModule } from 'src/weather/test/weather.module.test';
-import { CurrentOpenWeatherFetchDto, CurrentWeatherApiFetchDto } from 'src/weather/types/current-weather-api.type';
 import * as request from 'supertest';
 import { TestsUrl } from 'test/utils/test-urls.enum';
-
-const fetchWeatherResponseOK: CurrentWeatherApiFetchDto | null = {
-  location: { name: 'ValidCity', region: '', country: '' },
-  current: { temp_c: 0, humidity: 1, condition: { text: 'text' } },
-};
-
-const fetchWeatherResponseError: CurrentWeatherApiFetchDto = {
-  location: { name: 'Invalid', region: '', country: '' },
-  current: { temp_c: 0, humidity: 1, condition: { text: 'text' } },
-};
 
 const weatherResponse: CurrentWeatherResponseDto = {
   temperature: 0,
   humidity: 1,
   description: 'text',
-};
-
-const reserveWeatherResponse: CurrentOpenWeatherFetchDto = {
-  weather: [{ description: 'text' }],
-  main: { temp: 0, humidity: 1 },
-  name: 'ValidCity',
-};
-
-const mockHttpService: Partial<HttpService> = {
-  request: jest.fn(),
 };
 
 describe('WeatherContoller (Integration)', () => {
@@ -81,7 +60,7 @@ describe('WeatherContoller (Integration)', () => {
       // first provider succeeds
       jest.spyOn(primaryProvider as any, 'getCurrentWeather').mockResolvedValue(weatherResponse);
 
-      const response = await request(app.getHttpServer()).get(`${TestsUrl.WEATHER}?city=${fetchWeatherResponseOK.location.name}`).expect(HttpStatus.OK);
+      const response = await request(app.getHttpServer()).get(`${TestsUrl.WEATHER}?city=CityValid`).expect(HttpStatus.OK);
       expect(response.body).toMatchObject(weatherResponse);
     });
 
@@ -91,15 +70,15 @@ describe('WeatherContoller (Integration)', () => {
       // second provider succeeds
       jest.spyOn(secondaryProvider as any, 'getCurrentWeather').mockResolvedValue(weatherResponse);
 
-      const response = await request(app.getHttpServer()).get(`${TestsUrl.WEATHER}?city=${reserveWeatherResponse.name}`).expect(HttpStatus.OK);
+      const response = await request(app.getHttpServer()).get(`${TestsUrl.WEATHER}?city=CityValid`).expect(HttpStatus.OK);
 
       expect(response.body).toMatchObject(weatherResponse);
     });
 
     it('should throw CityNotFoundException if both providers fail', async () => {
       // both providers fail
-      jest.spyOn(primaryProvider as any, 'getCurrentWeather').mockResolvedValue(fetchWeatherResponseError);
-      jest.spyOn(secondaryProvider as any, 'getCurrentWeather').mockResolvedValue(fetchWeatherResponseError);
+      jest.spyOn(primaryProvider as any, 'getCurrentWeather').mockRejectedValue(new CityNotFoundException());
+      jest.spyOn(secondaryProvider as any, 'getCurrentWeather').mockRejectedValue(new CityNotFoundException());
 
       const response = await request(app.getHttpServer()).get(`${TestsUrl.WEATHER}?city=InvalidCity`).expect(HttpStatus.NOT_FOUND);
       expect(response.body).toHaveProperty('message', 'City not found');
