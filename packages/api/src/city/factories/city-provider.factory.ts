a775aee499783e4809fb7d9386c7ed03199629f7
Ivan Hushchin <oopsgu2006@gmail.com>
2025-07-03 03:10:40 +0300

    merge bugs fixes

--- packages/api/src/city/factories/city-provider.factory.ts
+++ packages/api/src/city/factories/city-provider.factory.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { CacheServiceInterface } from 'src/cache/interfaces/cache-service.interface';
 import { CityProviderAdapter } from 'src/common/adapters/city-provider.adapter';
 import { CityProviderLoggingDecorator } from 'src/common/decorators/city-provider-logging.decorator';
+import { CityProviderCacheProxy } from 'src/common/proxies/city-validation-cache.proxy';
 import { LoggerService } from 'src/logger/logger.service';
 import { CityProvider } from '../interfaces/city.provider';
 import { OpenWeatherCityProvider } from '../providers/openweather.provider';
@@ -12,13 +14,14 @@ export class CityProviderFactory {
     private readonly openWeatherProvider: OpenWeatherCityProvider,
     private readonly weatherApiProvider: WeatherApiCityProvider,
     private readonly logger: LoggerService,
+    private readonly cacheService: CacheServiceInterface,
   ) {}
 
   create(): CityProvider {
     const decoratedWeatherAPI = new CityProviderLoggingDecorator(this.weatherApiProvider, this.logger);
     const decoratedOpenWeather = new CityProviderLoggingDecorator(this.openWeatherProvider, this.logger);
-    const chain = decoratedWeatherAPI.setNext(decoratedOpenWeather);
+    const cacheProxied = new CityProviderCacheProxy(decoratedWeatherAPI.setNext(decoratedOpenWeather), this.cacheService);
 
-    return new CityProviderAdapter(chain);
+    return new CityProviderAdapter(cacheProxied);
   }
 }
