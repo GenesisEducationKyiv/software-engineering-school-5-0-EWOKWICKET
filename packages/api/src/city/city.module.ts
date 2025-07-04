7912148bfd430313c9224788cdcb93b7f0ca301d
Ivan Hushchin <oopsgu2006@gmail.com>
2025-07-04 16:32:02 +0300

    chain setup moved into modules

--- packages/api/src/city/city.module.ts
+++ packages/api/src/city/city.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CacheModule } from 'src/cache/cache.module';
+import { CacheService } from 'src/cache/cache.service';
+import { CityProviderAdapter } from 'src/common/adapters/city-provider.adapter';
+import { CityProviderLoggingDecorator } from 'src/common/decorators/city-provider-logging.decorator';
+import { CityProviderCacheProxy } from 'src/common/proxies/city-validation-cache.proxy';
 import { LoggerModule } from 'src/logger/logger.module';
+import { LoggerService } from 'src/logger/logger.service';
 import { CityExistsConstraint } from './city-exists.constraint';
-import { CityProviderFactory } from './factories/city-provider.factory';
 import { CityProvider } from './interfaces/city.provider';
 import { OpenWeatherCityProvider } from './providers/openweather.provider';
 import { WeatherApiCityProvider } from './providers/weatherapi.provider';
@@ -13,11 +17,16 @@ import { WeatherApiCityProvider } from './providers/weatherapi.provider';
     CityExistsConstraint,
     WeatherApiCityProvider,
     OpenWeatherCityProvider,
-    CityProviderFactory,
     {
       provide: CityProvider,
-      inject: [CityProviderFactory],
-      useFactory: (cityFactory: CityProviderFactory) => cityFactory.create(),
+      inject: [WeatherApiCityProvider, OpenWeatherCityProvider, LoggerService, CacheService],
+      useFactory: (weatherApiProvider: WeatherApiCityProvider, openWeatherProvider: OpenWeatherCityProvider, logger: LoggerService, cacheService: CacheService) => {
+        const decoratedWeatherAPI = new CityProviderLoggingDecorator(weatherApiProvider, logger);
+        const decoratedOpenWeather = new CityProviderLoggingDecorator(openWeatherProvider, logger);
+        const cachProxied = new CityProviderCacheProxy(decoratedWeatherAPI.setNext(decoratedOpenWeather), cacheService);
+
+        return new CityProviderAdapter(cachProxied);
+      },
     },
   ],
   exports: [CityExistsConstraint],
--- packages/api/src/weather/weather.module.ts
+++ packages/api/src/weather/weather.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CacheModule } from 'src/cache/cache.module';
+import { CacheService } from 'src/cache/cache.service';
+import { WeatherProviderAdapter } from 'src/common/adapters/weather-povider.adapter';
+import { WeatherProviderLoggingDecorator } from 'src/common/decorators/weather-provider-logging.decorator';
+import { WeatherProviderCacheProxy } from 'src/common/proxies/weather-cache.proxy';
 import { LoggerModule } from 'src/logger/logger.module';
-import { WeatherProviderFactory } from './factories/weather-provider.factory';
+import { LoggerService } from 'src/logger/logger.service';
 import { WeatherProvider } from './interfaces/current-weather.abstract';
 import { OpenWeatherWeatherProvider } from './providers/openweather.provider';
 import { WeatherApiWeatherProvider } from './providers/weatherapi.provider';
@@ -11,13 +15,18 @@ import { WeatherController } from './weather.controller';
   imports: [LoggerModule, CacheModule],
   controllers: [WeatherController],
   providers: [
-    WeatherProviderFactory,
     WeatherApiWeatherProvider,
     OpenWeatherWeatherProvider,
     {
       provide: WeatherProvider,
-      inject: [WeatherProviderFactory],
-      useFactory: (weatherFactory: WeatherProviderFactory) => weatherFactory.create(),
+      inject: [WeatherApiWeatherProvider, OpenWeatherWeatherProvider, LoggerService, CacheService],
+      useFactory: (weatherApiProvider: WeatherApiWeatherProvider, openWeatherProvider: OpenWeatherWeatherProvider, logger: LoggerService, cacheService: CacheService) => {
+        const decoratedWeatherAPI = new WeatherProviderLoggingDecorator(weatherApiProvider, logger);
+        const decoratedOpenWeather = new WeatherProviderLoggingDecorator(openWeatherProvider, logger);
+        const cachProxied = new WeatherProviderCacheProxy(decoratedWeatherAPI.setNext(decoratedOpenWeather), cacheService);
+
+        return new WeatherProviderAdapter(cachProxied);
+      },
     },
   ],
   exports: [WeatherProvider],
