afd2b8b818473b4abe2bcbf11ea6e5d284921aaf
Ivan Hushchin <oopsgu2006@gmail.com>
2025-07-18 04:24:01 +0300

    minor lint fixes

--- packages/api/apps/notifications/src/common/pipes/notification-type-validation.pipe.ts
+++ packages/api/apps/notifications/src/common/pipes/notification-type-validation.pipe.ts
@@ -3,6 +3,7 @@ import { Injectable, InternalServerErrorException, PipeTransform } from '@nestjs
 
 @Injectable()
 export class NotificationTypeValidationPipe implements PipeTransform {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   transform(value: any) {
     if (!Object.values(NotificationType).includes(value)) {
       throw new InternalServerErrorException(`Invalid notification type: ${value}`);
