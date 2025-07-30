1e7b18f806890d9ae6ab9171b8daa8b8ea6f6eac
Ivan Hushchin <oopsgu2006@gmail.com>
2025-07-30 14:25:53 +0300

    observability docs added

--- packages/api/libs/common/src/metrics/interfaces/red-metrics.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface REDMetricsService {
-  onRequestStart(transport: string, route: string): void;
-  onRequestEnd(transport: string, route: string, durationMs: number): void;
-  onRequestError(transport: string, route: string): void;
-}
